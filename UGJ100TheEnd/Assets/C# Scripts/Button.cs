65c0f395396aaa3f6bd20c4d6a11709bf3b060be
Jack Burrup <62547604+Quertxx@users.noreply.github.com>
2024-10-18 23:23:53 +0100

    Added one time button interactible.

--- UGJ100TheEnd/Assets/C# Scripts/Button.cs
+++ UGJ100TheEnd/Assets/C# Scripts/Button.cs
@@ -2,30 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressurePlate : MonoBehaviour
+public class Button : MonoBehaviour
 {
     [SerializeField]
     private GameObject InteractedObject;
+    private bool isPressed = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            InteractedObject.GetComponent<IInteractible>().Interact(gameObject);
-        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Body")
+        if (collision.gameObject.tag == "Body")
         {
-            InteractedObject.GetComponent<IInteractible>().Interact(gameObject);
+            if (!isPressed)
+            {
+                isPressed = true;
+                InteractedObject.GetComponent<IInteractible>().Interact(gameObject);
+            }
+            
 
         }
     }
