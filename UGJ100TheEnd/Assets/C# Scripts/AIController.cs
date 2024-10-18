fc1d691dfb3347b1b7764f6cc13ecee85358975f
Jack Burrup <62547604+Quertxx@users.noreply.github.com>
2024-10-18 20:13:01 +0100

    Basic AI combat, ranged and Melee

--- UGJ100TheEnd/Assets/C# Scripts/AIController.cs
+++ UGJ100TheEnd/Assets/C# Scripts/AIController.cs
@@ -20,9 +20,14 @@ public class AIController : MonoBehaviour, IDamagable
     [SerializeField] private int meleeStoppingDistance;
     [SerializeField] private int meleeHealth;
     [SerializeField] private float meleeSpeed;
+    [SerializeField] private float attackRange;
+    [SerializeField] LayerMask enemyLayers;
+    [SerializeField] Transform attackPoint;
+    [SerializeField] int attackDamage;
     private int curHealth;
     private int maxHealth;
     private bool isShooting;
+    private bool isHitting=false;
 
 
 
@@ -74,12 +79,30 @@ public class AIController : MonoBehaviour, IDamagable
                     if (isShooting)
                     {
                         isShooting = false;
-                        CancelInvoke();
+                        CancelInvoke("shootBullet");
                     }
                 }
                 break;
             case EnemyType.Melee:
                 //Should put attacking logic here
+                if(Vector3.Distance(gameObject.transform.position, playerCharacter.transform.position) <= 2f)
+                {
+                    if (!isHitting)
+                    {
+                        Debug.Log("Trying to hit?");
+                        isHitting = true;
+                        InvokeRepeating("meleeAttack", 0.1f, 1.0f);
+                    }
+
+                }
+                else
+                {
+                    if (isHitting)
+                    {
+                        isHitting = false;
+                        CancelInvoke("meleeAttack");
+                    }
+                }
                 break;
         }
     }
@@ -99,7 +122,15 @@ public class AIController : MonoBehaviour, IDamagable
         bulletRB.velocity = direction * bulletSpeed;
 
     }
-
+    private void meleeAttack()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+            enemy.GetComponent<IDamagable>().Damaged(attackDamage);
+        }
+    }
 
     enum EnemyType 
     {
