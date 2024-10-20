d4fb6d32cf14d3a8a077af871deda394e2215497
TyreseWelsh <79930058+TyreseWelsh@users.noreply.github.com>
2024-10-20 19:55:51 +0100

    Implemented stamina system
    I have created a new script called "StaminaComponent" which you can attach to any gameobject you want to have stamina. This script handles the consumption of stamina from the gameobject, stamina regen, and the function that you will call from the main character script returns an enum for the strength level of the ability you're using. Simply call the "ConsumeStamina" function in a switch statement, and implement the functionality for each strength level.

--- UGJ100TheEnd/Assets/UGJ/Entities/Player/Scripts/MainPlayerController.cs
+++ UGJ100TheEnd/Assets/UGJ/Entities/Player/Scripts/MainPlayerController.cs
@@ -1,83 +1,194 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MainPlayerController : MonoBehaviour
+public class MainPlayerController : MonoBehaviour, IDamagable
 {
+    [Header("Components")]
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject mesh;
     
-    [SerializeField] private float moveSpeed;
+    private CharacterController controller;
+    private StaminaComponent staminaComponent;
+    
+    [Header("Basic Stats")]
+    [SerializeField] private int maxHealth;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private int lives;
+
+    [Header("Dash Stats")]
     [SerializeField] private float dashSpeed;
     [SerializeField] private float dashDuration;
+    [SerializeField] private int dashCost;
     
-    CharacterController controller;
-    private Vector3 movementDirection;
+    [Header("")]
+    [SerializeField] private float reviveDuration;
 
+    [HideInInspector] public int currentHealth;
+    [HideInInspector] public float currentSpeed;
+
+    private Vector3 movementDirection;
+    private bool holdingCorpse = false;
+
+
+    public enum EHealthStates {Alive, Reviving, Dead}
+    [Header("")]
+    public EHealthStates currentHealthState = EHealthStates.Alive;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        staminaComponent = GetComponent<StaminaComponent>();
+        
+        currentHealth = maxHealth;
+        currentSpeed = maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(moveSpeed * Time.deltaTime * movementDirection);
+        if (currentHealthState != EHealthStates.Dead)
+        {
+            movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            controller.Move(currentSpeed * Time.deltaTime * movementDirection);
+        }
     }
 
     public void Look(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            // Getting the screen mouse position when the mouse is moved
-            Vector2 screenMousePosition = context.ReadValue<Vector2>();
+            if (currentHealthState != EHealthStates.Dead)
+            {
+                // Getting the screen mouse position when the mouse is moved
+                Vector2 screenMousePosition = context.ReadValue<Vector2>();
 
-            // 
-            float cameraToPlayerDistance = Mathf.Abs(mainCamera.transform.position.y - transform.position.y);
-            Vector3 mousePoint = mainCamera.ScreenToWorldPoint(new Vector3(screenMousePosition.x, screenMousePosition.y, cameraToPlayerDistance));
-            mousePoint.y = transform.position.y;
-        
-            mesh.transform.LookAt(mousePoint);
+                // 
+                float cameraToPlayerDistance = Mathf.Abs(mainCamera.transform.position.y - transform.position.y);
+                Vector3 mousePoint = mainCamera.ScreenToWorldPoint(new Vector3(screenMousePosition.x,
+                    screenMousePosition.y, cameraToPlayerDistance));
+                mousePoint.y = transform.position.y;
+
+                mesh.transform.LookAt(mousePoint);
+            }
         }
     }
-    
+
     public void StartDash(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            float dashStartTime = Time.time;
-            StartCoroutine(Dash(movementDirection, dashStartTime));
+            if (currentHealthState == EHealthStates.Alive)
+            {
+                float dashStartTime = Time.time;
+
+                switch (staminaComponent.ConsumeStamina(dashCost))
+                {
+                    case StaminaComponent.EStaminaAbilityStrength.Full:
+                        gameObject.layer = LayerMask.NameToLayer("PlayerInvincible");
+                        StartCoroutine(Dash(movementDirection, dashStartTime, dashDuration));
+                        break;
+                    case StaminaComponent.EStaminaAbilityStrength.Reduced:
+                        StartCoroutine(Dash(movementDirection, dashStartTime, dashDuration / 2));
+                        break;
+                    case StaminaComponent.EStaminaAbilityStrength.Zero:
+                        // Not enough stamina
+                        break;
+                    default:
+                        // Default: Full power
+                        StartCoroutine(Dash(movementDirection, dashStartTime, dashDuration));
+                        break;
+                }
+            }
         }
     }
     
-    IEnumerator Dash(Vector3 direction, float startTime)
+    IEnumerator Dash(Vector3 direction, float startTime, float duration)
     {
-        while (Time.time < startTime + dashDuration)
+        while (Time.time < startTime + duration)
         {
             controller.Move(dashSpeed * Time.deltaTime * direction);
             
             yield return null;
         }
         
+        gameObject.layer = LayerMask.NameToLayer("Player");
         controller.velocity.Set(0, 0, 0);
     }
+
+    // This function should work with any stamina ability. Simply call this function in a switch statement and have the switch cases be the enum options
 
     public void Interact(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            RaycastHit hit;
-            Physics.Raycast(transform.position, mesh.transform.forward * 2, out hit);
-            Debug.DrawRay(transform.position, mesh.transform.forward * 2, Color.red, 0.5f);
-
-            // if hit object is not null
-            if (hit.collider)
+            if (currentHealthState == EHealthStates.Alive)
             {
-                hit.collider.gameObject.GetComponent<IInteractible>().Interact(gameObject);
+                // temp: testing revive
+                Damaged(2);
+                //
+            
+                RaycastHit hit;
+                Physics.Raycast(transform.position, mesh.transform.forward * 2, out hit);
+                Debug.DrawRay(transform.position, mesh.transform.forward * 2, Color.red, 0.5f);
+
+                // if hit object is not null
+                if (hit.collider)
+                {
+                    hit.collider.gameObject.GetComponent<IInteractible>().Interact(gameObject);
+                }
+            }
+        }
+    }
+
+    void StartRevive()
+    {
+        currentHealthState = EHealthStates.Reviving;
+        gameObject.layer = LayerMask.NameToLayer("PlayerInvincible");
+        currentSpeed /= 2;
+        StartCoroutine(ReviveCoroutine(Time.time));
+    }
+
+    IEnumerator ReviveCoroutine(float startTime)
+    {
+        while (Time.time < startTime + reviveDuration)
+        {
+            //Debug.Log("Reviving...");
+            yield return null;
+        }
+        
+        Revive();
+    }
+
+    void Revive()
+    {
+        currentHealth = maxHealth;
+        currentSpeed = maxSpeed;
+        gameObject.layer = LayerMask.NameToLayer("Player");
+        
+        currentHealthState = EHealthStates.Alive;
+        //Debug.Log("REVIVED");
+    }
+    
+    public void Damaged(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            lives--;
+
+            if (lives > 0)
+            {
+                StartRevive();
+            }
+            else
+            {
+                Debug.Log("NO PLAYER LIVES REMAINING - Restart game!");
+                currentHealthState = EHealthStates.Dead;
             }
         }
     }
---
+++ UGJ100TheEnd/Assets/UGJ/Entities/Player/Scripts/StaminaComponent.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StaminaComponent : MonoBehaviour
+{
+    public enum EStaminaAbilityStrength {Full, Reduced, Zero}
+
+    [Header("Stamina Stats")]
+    [SerializeField] private int maxStamina = 120;
+    [HideInInspector] public int currentStamina;
+    
+    [SerializeField] private float regenDelayTime = 2f;
+    [SerializeField] private int regenAmount = 2;
+    [SerializeField] private float regenRate = 0.1f;
+    private Coroutine delayCoroutine;
+    private Coroutine regenCoroutine;
+
+    [Header("Debug")]
+    [SerializeField] private bool debugEnabled = false;
+
+    private void Start()
+    {
+        currentStamina = maxStamina;
+    }
+
+    void StartRegenDelay()
+    {
+        if (debugEnabled)
+        {
+            print("Stamina is now = " + currentStamina);
+
+        }
+
+        if (delayCoroutine != null)
+        {
+            StopCoroutine(delayCoroutine);
+        }
+        StartCoroutine(RegenDelay());
+    }
+    
+    IEnumerator RegenDelay()
+    {
+        if (regenCoroutine != null)
+        {
+            StopCoroutine(regenCoroutine);
+        }
+        yield return new WaitForSeconds(regenDelayTime);
+        
+        regenCoroutine = StartCoroutine(RegenStamina());
+    }
+    
+    IEnumerator RegenStamina()
+    {
+        while (currentStamina < maxStamina)
+        {
+            currentStamina += regenAmount;
+            if (debugEnabled)
+            {
+                print("Stamina = " + currentStamina);
+            }
+            
+            yield return new WaitForSeconds(regenRate);
+            yield return null;
+        }
+        
+        currentStamina = maxStamina;
+    }
+    
+    // This function should be called from the gameobject script that handles the stamina ability
+    // It will handle reducing the gameobjects stamina based on the given cost and start a delay for its stamina regen
+    public EStaminaAbilityStrength ConsumeStamina(int staminaCost)
+    {
+        // If player has enough stamina, use ability at full strength
+        if (currentStamina >= staminaCost)
+        {
+            currentStamina -= staminaCost;
+            StartRegenDelay();
+            return EStaminaAbilityStrength.Full;
+        }
+
+        // If the stamina cost would reduce player stamina to negaitve half of max stamina, the ability wont be used
+        // Allowing the player to go into negative stamina gives them a bit of leeway, however there is a limit to how far negative they can go
+        if (currentStamina - staminaCost < -(maxStamina / 2))
+        {
+            return EStaminaAbilityStrength.Zero;
+        }
+
+        // If the above "if" statements dont apply, use ability at reduced strength
+        currentStamina -= staminaCost;
+        StartRegenDelay();
+        return EStaminaAbilityStrength.Reduced;
+    }
+}
